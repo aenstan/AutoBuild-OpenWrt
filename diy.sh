a12332c4fc24b1d26d840288cdc4b3c43f564527
aenstan <54591257+aenstan@users.noreply.github.com>
2020-04-10 18:57:27 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-#1. Modify default IP
+#diy config
 sed -i 's/192.168.1.1/192.168.0.1/g' openwrt/package/base-files/files/bin/config_generate
 git clone https://github.com/tty228/luci-app-serverchan.git package/luci-app-serverchan
 git clone https://github.com/vernesong/OpenClash.git package/OpenClash
 git clone https://github.com/LuttyYang/luci-theme-material.git package/luci-theme-material
+./scripts/feeds update -a
+./scripts/feeds install -a
