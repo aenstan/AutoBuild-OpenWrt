23bf55190274a1de1dfe5e7ce16cebee789bb9ed
aenstan <54591257+aenstan@users.noreply.github.com>
2020-04-14 22:30:22 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -4,6 +4,6 @@ sed -i 's/192.168.1.1/192.168.0.1/g' openwrt/package/base-files/files/bin/config
 git clone https://github.com/tty228/luci-app-serverchan.git package/luci-app-serverchan
 git clone https://github.com/vernesong/OpenClash.git package/OpenClash
 git clone https://github.com/LuttyYang/luci-theme-material.git package/luci-theme-material
-git clone https://github.com/tindy2013/openwrt-subconverter.git package/openwrt-subconverter
+git clone https://github.com/aenstan/openwrt-subconverter.git package/openwrt-subconverter
 ./scripts/feeds update -a
 ./scripts/feeds install -a
